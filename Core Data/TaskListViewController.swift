b562904aefe29919d16ea08d8bf4940c999c398a
Sergey <kopgamerk@gmail.com>
2022-04-25 21:27:16 +0300

    Work w/ coreData

--- Core Data/TaskListViewController.swift
+++ Core Data/TaskListViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import CoreData
 
-class TaskListViewController: UITableViewController {
+protocol TableViewReload {
+    func reloadData()
+}
 
+class TaskListViewController: UITableViewController, TableViewReload {
+    
+    
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private let cellID = "task"
+    
+    private var taskList: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "task")
         setupNavigationBar()
+        fetchData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
     }
     
     private func setupNavigationBar() {
@@ -26,9 +44,56 @@ class TaskListViewController: UITableViewController {
         navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.standardAppearance = navBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
-
+        
+        
+    }
+    
+    @objc private func addNewTask() {
+        let taskVC = TaskViewController()
+        taskVC.delegate = self
+        present(taskVC, animated: true)
+    }
+    
+    private func fetchData() {
+        let fetchRequest = Task.fetchRequest()
+        do {
+            taskList = try context.fetch(fetchRequest)
+        } catch let error {
+            print("failed to fetch data", error)
+        }
     }
 }
 
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = task.titleOfTask
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+extension TaskListViewController {
+    func reloadData() {
+        fetchData()
+        tableView.reloadData()
+    }
+}
---
+++ Core Data/TaskVC.swift
@@ -0,0 +1,116 @@
+//
+//  TaskVC.swift
+//  Core Data
+//
+//  Created by Сергей Веретенников on 25/04/2022.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+
+class TaskViewController: UIViewController {
+    
+    var delegate: TableViewReload?
+    
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "New task"
+        textField.borderStyle = .roundedRect
+        
+        
+        return textField
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 1)
+        button.setTitle("Save task", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 4
+        button.addTarget(self, action: #selector(save), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 225/255, green: 70/255, blue: 1/255, alpha: 1)
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 4
+        button.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupViews(textField, saveButton, cancelButton)
+        
+        setConstraints()
+        
+    }
+    
+    private func setupViews(_ subview: UIView...) {
+        subview .forEach { subview in
+            view.addSubview(subview)
+        }
+    }
+    
+    
+    private func setConstraints() {
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 20),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -240)
+        ])
+        
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 20),
+            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 240),
+            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+    }
+    
+    @objc private func save() {
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        guard let task = NSManagedObject(entity: entity, insertInto: context) as? Task else { return }
+        task.titleOfTask = textField.text
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error)
+            }
+        }
+        delegate?.reloadData()
+        dismiss(animated: true)
+    }
+    
+    
+    
+    @objc private func cancel() {
+        dismiss(animated: true)
+    }
+}
+
