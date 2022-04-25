e1c262b10bce57a42be34e5576ed12ed5e36258a
Sergey <kopgamerk@gmail.com>
2022-04-25 18:36:35 +0300

    Setup navBar

---
+++ Core Data/TaskListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Core Data
+//
+//  Created by Сергей Веретенников on 25/04/2022.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        
+        navBarAppearance.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 192/255, alpha: 0.8)
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+
+    }
+}
+
